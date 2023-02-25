5cb7d52290793ef7d9306dbd9d6871bc83a57429
KabilanRajendra <121474513+KabilanRajendra@users.noreply.github.com>
2023-02-25 00:14:44 -0800

    Edubridge mini project

---
+++ Online_Book_Store/src/com/Book/test/MySqlConnection.java
@@ -0,0 +1,39 @@
+package com.Book.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+	static Connection conn=null;
+	public static Connection getConnection() {
+		// TODO Auto-generated method stub
+	
+		try
+		{
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineBookStoredb", "root", "Kabilan@123");			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		return conn;
+	}
+	public static void closeConnection()
+	{
+		if(conn!=null)
+		{
+			try 
+			{
+				conn.close();
+			} 
+			catch (SQLException e) {
+
+				System.out.println("Something went wrong!!");
+			}
+		}
+	}
+}
