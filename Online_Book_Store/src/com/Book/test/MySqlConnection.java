d3cc0a0d455fd4a82399149e368fd3d5848931af
KabilanRajendra <121474513+KabilanRajendra@users.noreply.github.com>
2023-03-02 23:26:09 -0800

    Update MySqlConnection.java

--- Online_Book_Store/src/com/Book/test/MySqlConnection.java
+++ Online_Book_Store/src/com/Book/test/MySqlConnection.java
@@ -5,34 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnection {
-	static Connection conn=null;
-	public static Connection getConnection() {
-		// TODO Auto-generated method stub
 	
+	static Connection conn=null;
+	public static Connection getConnection()
+	{
+		
 		try
 		{
-			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineBookStoredb", "root", "Kabilan@123");			
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineBookStore", "root", "Kabilan@123");
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
 		}
-
 		return conn;
+		
 	}
+	
 	public static void closeConnection()
 	{
 		if(conn!=null)
 		{
-			try 
+			try
 			{
 				conn.close();
-			} 
-			catch (SQLException e) {
-
-				System.out.println("Something went wrong!!");
+			}
+			catch(SQLException e)
+			{
+				System.out.println("Something went wrong!!!");
 			}
 		}
 	}
